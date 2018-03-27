be2530980cd74c43776112055cf1710685b218ca
Joakim Wassberg <joakim@arthead.se>
2018-03-27 15:49:27 +0200

    Initial commit

---
+++ AsyncExample/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallWithAsync();
+            CallWithAsync2();
+            Console.WriteLine("Waiting for greeting");
+            Console.WriteLine("Taking a nap");
+            Thread.Sleep(2100);
+            Console.WriteLine("Back again.... Boring taking a nap again");
+            Thread.Sleep(2000);
+            Console.WriteLine("Ok I'm done");
+
+
+            Console.WriteLine("\n\nNext task, call async with combinators");
+            MultipleAsyncMethodsWithCombinators();
+            Console.WriteLine("Again, I will sleep");
+            Thread.Sleep(2000);
+            Console.WriteLine("And some more...");
+            Thread.Sleep(2000);
+            Console.WriteLine("Now I'm done");
+           // var result = Greeting("Anna", 2000);
+           // Console.WriteLine(result);
+        }
+
+        private async static void MultipleAsyncMethodsWithCombinators()
+        {
+            Task<string> t1 = GreetingAsync("Peter", 2000);
+            Task<string> t2 = GreetingAsync("Mary", 3000);
+
+            await Task.WhenAll(t1, t2);
+            Console.WriteLine("Both tasks done...");
+            Console.WriteLine("Result 1: {0}\nResult 2: {1}", t1.Result, t2.Result);
+        }
+
+        private async static void CallWithAsync()
+        {
+            string result1 = await GreetingAsync("Anna", 2000);
+            Console.WriteLine(result1);
+
+        }
+
+        private async static void CallWithAsync2()
+        {
+            string result2 = await GreetingAsync("Simon", 1500);
+            Console.WriteLine(result2);
+        }
+
+        private static Task<string> GreetingAsync(string name, int sleepTimeMs)
+        {
+            return Task.Run<string>(() =>
+            {
+                return Greeting(name, sleepTimeMs);
+            });
+        }
+
+        static string Greeting(string name, int sleepTimeMs)
+        {
+            Thread.Sleep(sleepTimeMs);
+            return string.Format("Hello, {0}", name);
+        }
+    }
+}
---
+++ ThreadPool/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPoolExample
+{
+    public class Fibonacci
+    {
+        public Fibonacci(long n, ManualResetEvent doneEvent)
+        {
+            _n = n;
+            _doneEvent = doneEvent;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Calculating fib of {0}...", _n);
+            _fibOfN = Calculate(_n);
+            Console.WriteLine("Result is {0}", _fibOfN);
+        }
+
+        public void ThreadPoolCallback(Object threadContext)
+        {
+            int threadIndex = (int)threadContext;
+            Console.WriteLine("Thread {0} started...", threadIndex);
+            _fibOfN = Calculate(_n);
+            Console.WriteLine("Thread {0} result calculated", threadIndex);
+            _doneEvent.Set();
+        }
+
+        public long Calculate(long n)
+        {
+            if( n <= 1)
+            {
+                return n;
+            }
+            return Calculate(n - 1) + Calculate(n - 2);
+        }
+
+        private long _n;
+        public long N { get { return _n; } }
+
+        private long _fibOfN;
+        public long FibOfN { get { return _fibOfN; } }
+
+        private ManualResetEvent _doneEvent;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int FibonacciCalculations = 10;
+            ManualResetEvent[] doneEvents = new ManualResetEvent[FibonacciCalculations];
+            Fibonacci[] fibArray = new Fibonacci[FibonacciCalculations];
+            Random r = new Random();
+
+            Console.WriteLine("Launching {0} tasks...", FibonacciCalculations);
+            for(int i = 0; i < FibonacciCalculations; i++)
+            {
+                doneEvents[i] = new ManualResetEvent(false);
+                Fibonacci f = new Fibonacci(r.Next(20, 45), doneEvents[i]);
+                fibArray[i] = f;
+                ThreadPool.QueueUserWorkItem(f.ThreadPoolCallback, i);
+            }
+            WaitHandle.WaitAll(doneEvents);
+            Console.WriteLine("All calculations are complete.");
+            for(int i = 0; i < FibonacciCalculations; i++)
+            {
+                Fibonacci f = fibArray[i];
+                Console.WriteLine("Fiboancci({0}) = {1}", f.N, f.FibOfN);
+            }
+        }
+    }
+}
